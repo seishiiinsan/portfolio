d7e0f8f36cf6a902276ea4c155b36257ec183b71
seishiiinsan <gabinhalloss@gmail.com>
2026-07-01 14:02:44 +0200

    marquee ticker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@ import About from "@/components/sections/About";
 import Experience from "@/components/sections/Experience";
 import Work from "@/components/sections/Work";
 import Contact from "@/components/sections/Contact";
+import Marquee from "@/components/ui/Marquee";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <Marquee />
       <Experience />
       <Work />
+      <Marquee />
       <Contact />
     </>
   );
